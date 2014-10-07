327fb865c2fc393f8ea36b2c2528c4c62f94959c
Keivan Beigi <me@keivan.io>
2014-10-07 11:50:18 -0700

    started to add Argument parsing

---
+++ src/AppGet.Tests/ArgumentsFixture.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace AppGet.Tests
+{
+    [TestFixture]
+    public class ArgumentsFixture
+    {
+        [TestCase("install /t /c=12 /awd")]
+        [TestCase("INSTALL /t /c=12 /awd")]
+        public void should_parse_command_name(string args)
+        {
+            var arguments = new Arguments(args);
+            arguments.Command.Should().Be("install");
+        }
+
+        [TestCase("install /t /c=12 /awd")]
+        [TestCase("INSTALL -t -c=12 =awd")]
+        [TestCase("INSTALL /t /awd")]
+        [TestCase("INSTALL -t -awd")]
+        [TestCase("INSTALL -t /awd")]
+        [TestCase("INSTALL /t /awd /url:http://test")]
+        [TestCase("INSTALL -t -awd -url:http://test")]
+        public void should_parse_flags_name(string args)
+        {
+            var arguments = new Arguments(args);
+            arguments.Flags.Should().HaveCount(2);
+            arguments.Flags.Should().Contain("t");
+            arguments.Flags.Should().Contain("awd");
+        }
+
+    }
+}
--- src/AppGet/Arguments.cs
+++ src/AppGet/Arguments.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace AppGet
 {
     public class Arguments
     {
+        private static readonly Regex CommandNameRegex = new Regex(@"^\w+\s", RegexOptions.Compiled);
+
         public Arguments(string arguments)
         {
+            Flags = new HashSet<string>();
+            Params = new Dictionary<string, string>();
+
+            arguments = arguments.Trim();
+
+            var commandName = CommandNameRegex.Match(arguments);
+
+            if (commandName.Success)
+            {
+                Command = commandName.Value.Trim().ToLower();
+            }
 
         }
 
