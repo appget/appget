922d677b4d3e1c547cbc5bde4389d0834752f107
Keivan Beigi <me@keivan.io>
2014-10-06 23:12:21 -0700

    added application LifeTime to track delays

--- src/AppGet.Core/Application.cs
+++ src/AppGet.Core/Application.cs
@@ -1,13 +1,25 @@
-﻿using TinyIoC;
+﻿using System;
+using System.Diagnostics;
+using TinyIoC;
 
 namespace AppGet
 {
     public static class Application
     {
+        private static readonly Stopwatch Stopwatch = Stopwatch.StartNew();
+
+        public static TimeSpan ApplicationLifetime
+        {
+            get
+            {
+                return Stopwatch.Elapsed;
+            }
+        }
 
         public static int Main(string[] args)
         {
             var container = new TinyIoCContainer();
+            container.AutoRegister();
             return 0;
         }
     }
