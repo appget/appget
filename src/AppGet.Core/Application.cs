51e11b40f1f0bb943ac3da9f434af7defd379e56
Keivan Beigi <me@keivan.io>
2014-10-06 23:03:24 -0700

    Added TinyIoC

--- src/AppGet.Core/Application.cs
+++ src/AppGet.Core/Application.cs
@@ -1,9 +1,13 @@
-﻿namespace AppGet
+﻿using TinyIoC;
+
+namespace AppGet
 {
     public static class Application
     {
+
         public static int Main(string[] args)
         {
+            var container = new TinyIoCContainer();
             return 0;
         }
     }
