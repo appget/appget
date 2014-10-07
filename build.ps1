4e61343b37e17786bfc854c501d0ecd329da94f6
Keivan Beigi <me@keivan.io>
2014-10-06 23:29:20 -0700

    added build.ps1

---
+++ build.ps1
@@ -0,0 +1,31 @@
+$msBuild = 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe'
+
+Function Build()
+{
+    Write-Host "##teamcity[progressStart 'Build']"
+
+    $clean = $msbuild + " src\AppGet.sln /t:Clean /m"
+    $build = $msbuild + " src\AppGet.sln /p:Configuration=Release /t:Build /m"
+       
+    Invoke-Expression $clean
+    CheckExitCode
+
+    Invoke-Expression $build
+    CheckExitCode
+
+
+    Write-Host "##teamcity[progressFinish 'Build']"    
+}
+
+
+Function CheckExitCode()
+{
+        if ($lastexitcode -ne 0)
+        {
+            Write-Host $errorMessage
+            exit 1
+        }
+}
+
+
+Build
