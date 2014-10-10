972606dfb5598f456c0b4e5030df8894e327b9c9
Keivan Beigi <me@keivan.io>
2014-10-09 17:02:07 -0700

    added fastjson

---
+++ src/AppGet/Serialization/fastJSON/Getters.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace AppGet.Serialization.fastJSON
+{
+    public sealed class DatasetSchema
+    {
+        public List<string> Info ;//{ get; set; }
+        public string Name ;//{ get; set; }
+    }
+}
