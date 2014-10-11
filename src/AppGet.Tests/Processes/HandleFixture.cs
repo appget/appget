da4876658dc595e8d3d263d83757ac7b65ec732a
Keivan Beigi <me@keivan.io>
2014-10-10 17:59:42 -0700

    initial version of Handle

---
+++ src/AppGet.Tests/Processes/HandleFixture.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using AppGet.Processes;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AppGet.Tests.Processes
+{
+    [TestFixture]
+    public class HandleFixture
+    {
+        [TestCase]
+        public void should_get_locking_process()
+        {
+            var tempFile = Path.GetTempFileName();
+            File.OpenWrite(tempFile);
+            var processes = Handle.GetLockers(tempFile);
+            processes.Should().NotBeNull();
+        }
+    }
+}
