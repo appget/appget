9d8f5a2c1e7308cc38ee8d5cee62e1a44f2e3bc3
Kayone <me@keivan.io>
2018-02-08 08:00:31 -0800

    Lib cleanup

--- src/AppGet/Properties/AssemblyInfo.cs
+++ src/AppGet/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AppGet.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("AppGet")]
+[assembly: AssemblyDescription("Open Package Manager for Windows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AppGet.Core")]
+[assembly: AssemblyCompany("AppGet")]
+[assembly: AssemblyProduct("AppGet")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
