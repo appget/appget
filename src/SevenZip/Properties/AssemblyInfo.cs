689bc18ff8a78448dce0ad73c51f0e7a815047cf
Kayone <me@keivan.io>
2018-02-16 13:21:18 -0800

    Added SevenZip

---
+++ src/SevenZip/Properties/AssemblyInfo.cs
@@ -0,0 +1,55 @@
+﻿/*  This file is part of SevenZipSharp.
+
+    SevenZipSharp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SevenZipSharp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with SevenZipSharp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("SevenZipSharp")]
+[assembly: AssemblyDescription("7-zip native library wrapper")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Markovtsev Vadim")]
+[assembly: AssemblyProduct("SevenZipSharp")]
+[assembly: AssemblyCopyright("Copyright (C) Markovtsev Vadim 2009, 2010, licenced under LGPLv3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("80ed772d-f5a7-4314-b5bb-2be78e5f5f06")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("0.64.*")]
