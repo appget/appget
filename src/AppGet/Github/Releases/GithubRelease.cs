85c5e1822d8c52c0d87937dfd64d02425e2a0fd3
Kayone <me@keivan.io>
2018-03-02 01:13:18 -0800

    Fixed

---
+++ src/AppGet/Github/Releases/GithubRelease.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+// ReSharper disable once InconsistentNaming
+namespace AppGet.Github.Releases
+{
+    public class GithubRelease
+    {
+        public string tag_name { get; set; }
+        public List<GithubReleaseAsset> Assets { get; set; }
+    }
+
+    public class GithubReleaseAsset
+    {
+        public string browser_download_url { get; set; }
+    }
+}
