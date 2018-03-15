aababd04bb53150aff193bd79bc70d36e225b5cc
Kayone <me@keivan.io>
2018-03-14 18:43:38 -0700

    fixed broken test

--- src/AppGet.Tests/ProgressTracker/ProgressStateFixture.cs
+++ src/AppGet.Tests/ProgressTracker/ProgressStateFixture.cs
@@ -6,17 +6,17 @@ namespace AppGet.Tests.ProgressTracker
 {
     [TestFixture]
     public class ProgressStateFixture
-    {  
+    {
         [Test]
         public void should_return_correct_percent()
         {
             var state = new ProgressState
             {
-                Completed = 5,
-                Total = 10
+                Completed = 10,
+                Total = 20
             };
 
-            state.PercentCompleted.Should().Be(50);
+            state.ToString().Should().Contain("50");
         }
     }
 }
