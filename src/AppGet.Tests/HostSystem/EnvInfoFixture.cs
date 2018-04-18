c62161b01083b2f0508ab769b93b0f25a8ce44c9
Kayone <me@keivan.io>
2018-04-18 09:20:46 -0700

    Opps

--- src/AppGet.Tests/HostSystem/EnvInfoFixture.cs
+++ src/AppGet.Tests/HostSystem/EnvInfoFixture.cs
@@ -15,7 +15,7 @@ namespace AppGet.Tests.HostSystem
             Subject.Name.Should().Contain("Windows");
             Subject.Version.Major.Should().BeGreaterThan(7);
             Subject.FullName.Should().Contain("Windows NT");
-            Subject.FullName.Should().Contain(Subject.Name);
+            Subject.FullName.Should().Contain(Subject.Version.ToString());
 
             Subject.Is64BitOperatingSystem.Should().BeTrue();
             Subject.AppDir.Should().Contain(Path.DirectorySeparatorChar.ToString());
