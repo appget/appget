ca6318617d1e38f331a6130a62cc7ac270aa135a
Keivan Beigi <me@keivan.io>
2018-06-15 17:27:50 -0700

    Added PackageManifestExt

---
+++ src/AppGet.Manifest/PackageManifestExt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using AppGet.Manifest.Builder;
+using YamlDotNet.Serialization;
+
+namespace AppGet.Manifest
+{
+    public static class PackageManifestExt
+    {
+        public static Uri Uri(this Installer installer)
+        {
+            return new Uri(installer.Location);
+        }
+
+        public static Uri Uri(this InstallerBuilder installer)
+        {
+            return new Uri(installer.Location);
+        }
+    }
+}
