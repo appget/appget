d683fbc4f60bc4212813c138b3947fd4a7ea67e9
Keivan <me@keivan.io>
2018-08-14 10:51:40 -0700

    Allow options to be passed using / parsing output

--- src/AppGet/Commands/OptionsParser.cs
+++ src/AppGet/Commands/OptionsParser.cs
@@ -1,31 +1,97 @@
-﻿using AppGet.Commands.CreateManifest;
+﻿using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using AppGet.Commands.CreateManifest;
 using AppGet.Commands.Install;
 using AppGet.Commands.Outdated;
 using AppGet.Commands.Search;
 using AppGet.Commands.Uninstall;
 using AppGet.Commands.Update;
 using AppGet.Commands.ViewManifest;
+using AppGet.Extensions;
 using CommandLine;
+using JetBrains.Annotations;
+using Console = Colorful.Console;
 
 namespace AppGet.Commands
 {
+    public class CustomHelpWriter : TextWriter
+    {
+        public override Encoding Encoding { get; }
+
+        public override void Write(string value)
+        {
+            var lines = Regex.Split(value, "\r\n|\r|\n");
+
+            if (lines.Length < 3)
+            {
+                foreach (var line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+                return;
+            }
+
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("Copyright")) continue;
+                if (line.StartsWith("AppGet ")) continue;
+
+                if (line.StartsWith("ERROR"))
+                {
+                    Console.WriteLine("");
+                    continue;
+                }
+
+                if (line.StartsWith("  ") || line.IsNullOrWhiteSpace())
+                {
+                    Console.WriteLine(line);
+                    continue;
+                }
+
+                Console.WriteLine(line, Color.Red);
+            }
+        }
+    }
+
     public interface IParseOptions
     {
         AppGetOption Parse(params string[] args);
     }
 
+    [UsedImplicitly]
     public class OptionsParser : IParseOptions
     {
         public AppGetOption Parse(params string[] args)
         {
-            var result = Parser.Default.ParseArguments<
+            var cleanArgs = args.Select(c =>
+            {
+                if (c.StartsWith("/"))
+                {
+                    return "-" + c.Remove(0, 1);
+                }
+
+                return c;
+            });
+
+            var parser = new Parser(c =>
+            {
+                c.CaseSensitive = true;
+                c.IgnoreUnknownArguments = false;
+                c.HelpWriter = new CustomHelpWriter();
+            });
+
+            var result = parser.ParseArguments<
                 InstallOptions,
                 SearchOptions,
                 UninstallOptions,
                 CreateManifestOptions,
                 ViewManifestOptions,
                 OutdatedOptions,
-                UpdateOptions>(args);
+                UpdateOptions>(cleanArgs);
 
             if (result.Tag == ParserResultType.Parsed)
             {
