31fd6bc3e9f8f404cd8f3b79baee3dff0a3cf310
Keivan <me@keivan.io>
2018-08-19 21:23:51 -0700

    BlankWindow (MUI)

--- src/AppGet.Gui/ShellView.xaml.cs
+++ src/AppGet.Gui/ShellView.xaml.cs
@@ -1,10 +1,26 @@
-﻿namespace AppGet.Gui
+﻿using System;
+using System.Windows;
+using Caliburn.Micro;
+using ModernUI;
+
+namespace AppGet.Gui
 {
     public partial class ShellView
     {
         public ShellView()
         {
             InitializeComponent();
+
+            // don't do anything in design mode
+            if (ModernUIHelper.IsInDesignMode)
+            {
+                return;
+            }
+
+            var view = (DependencyObject)System.Windows.Application.LoadComponent(new Uri(@"\CaliburnShellView.xaml", UriKind.Relative));
+            var viewModel = ViewModelLocator.LocateForView(view);
+            ViewModelBinder.Bind(viewModel, view, null);
+            Content = view;
         }
     }
 }
