ad4069e89134275b6167b23b17c875a8a92f37b7
Keivan <me@keivan.io>
2018-09-01 11:22:22 -0700

    Added AppGetApiException

---
+++ src/AppGet/Http/AppGetApiError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AppGet.Http
+{
+    public class AppGetApiError
+    {
+        public string Message { get; set; }
+        public Version Ver { get; set; }
+        public string Tracking { get; set; }
+        public string Host { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Message}; code: {Tracking}";
+        }
+    }
+}
---
+++ src/AppGet/Http/AppGetApiException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace AppGet.Http
+{
+    public class AppGetApiException : Exception
+    {
+        public HttpResponseMessage Response { get; }
+        public HttpRequestMessage Request { get; }
+        public AppGetApiError ApiError { get; }
+
+        private string body;
+
+        public AppGetApiException(HttpResponseMessage response)
+        {
+            Response = response;
+            Request = response.RequestMessage;
+
+            ApiError = response.Deserialize<AppGetApiError>().Result;
+        }
+
+        public override string Message => $"{Response.StatusCode.ToString()}: {Request.RequestUri}. {ApiError}";
+    }
+}
