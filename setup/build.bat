78f919d9e2553e3f84c68d04ce11d1032b8f20f3
Keivan <me@keivan.io>
2018-09-18 18:49:59 -0700

    Added installer signing

--- setup/build.bat
+++ setup/build.bat
@@ -3,3 +3,5 @@ REM SET APPVEYOR_BUILD_VERSION=1.0.0
 del src\AppGet\bin\x86\Release\*.xml
 setup\inno\ISCC.exe setup\appget.iss
 
+
+setup\sign.bat
---
+++ setup/sign.bat
@@ -0,0 +1,11 @@
+setup\AzureSignTool\AzureSignTool.exe sign ^
+--description-url "https://appget.net" ^
+--file-digest sha512 ^
+--azure-key-vault-url %AKV_URL% ^
+--azure-key-vault-client-id %AKV_CLIENT_ID% ^
+--azure-key-vault-client-secret %AKV_CLIENT_SECRET% ^
+--azure-key-vault-certificate appget-software ^
+--timestamp-rfc3161 http://timestamp.digicert.com ^
+--timestamp-digest sha512 ^
+-v ^
+setup\Output\appget.%APPVEYOR_BUILD_VERSION%.exe
