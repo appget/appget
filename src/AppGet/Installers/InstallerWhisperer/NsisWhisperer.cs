c13dcc8a08ea401c290569119c023a0a1901de94
Keivan <me@keivan.io>
2018-11-12 14:42:02 -0800

    Added ExitCodes for NSIS

--- src/AppGet/Installers/InstallerWhisperer/NsisWhisperer.cs
+++ src/AppGet/Installers/InstallerWhisperer/NsisWhisperer.cs
@@ -1,4 +1,5 @@
-﻿using AppGet.Manifest;
+﻿using System.Collections.Generic;
+using AppGet.Manifest;
 
 namespace AppGet.Installers.InstallerWhisperer
 {
@@ -9,5 +10,15 @@ namespace AppGet.Installers.InstallerWhisperer
         public override string PassiveArgs => null;
         public override string SilentArgs => "/S";
         public override string LogArgs { get; }
+
+        public override Dictionary<int, ExistReason> ExitCodes => new Dictionary<int, ExistReason>
+        {
+            {
+                1, new ExistReason(ExitCodeTypes.UserCanceled)
+            },
+            {
+                2, new ExistReason(ExitCodeTypes.Failed, "Installation aborted by script")
+            }
+        };
     }
 }
