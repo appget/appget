0c49217716a905d95a0213444cbc47bfca15a35e
Keivan <me@keivan.io>
2018-11-16 10:25:24 -0800

    Added JsonClone

---
+++ src/AppGet.Manifest.Tests/Serialization/JsonCloneTests.cs
@@ -0,0 +1,21 @@
+﻿using AppGet.Manifest.Serialization;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace AppGet.Manifest.Tests.Serialization
+{
+    [TestFixture]
+    public class JsonCloneTests
+    {
+
+        [Test]
+        public void clone_should_create_new_instance()
+        {
+            var manifest = new PackageManifest { Name = "ABC", Id = "abc" };
+
+            var clone = manifest.Clone();
+            clone.Should().BeEquivalentTo(manifest);
+            clone.Should().NotBe(manifest);
+        }
+    }
+}
---
+++ src/AppGet.Manifest/Serialization/JsonClone.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AppGet.Manifest.Serialization
+{
+    public static class JsonClone
+    {
+        public static T Clone<T>(this T source) where T : new()
+        {
+            var json = Json.Serialize(source);
+            return Json.Deserialize<T>(json);
+        }
+    }
+}
