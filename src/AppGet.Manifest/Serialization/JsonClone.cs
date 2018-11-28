eba016756fefa53e59dcb157b23a743a6151cd99
Keivan <me@keivan.io>
2018-11-28 13:41:09 -0800

    switched JsonClone to use it's own independent serializer settings

--- src/AppGet.Manifest/Serialization/JsonClone.cs
+++ src/AppGet.Manifest/Serialization/JsonClone.cs
@@ -1,12 +1,23 @@
-﻿
+﻿using Newtonsoft.Json;
+
 namespace AppGet.Manifest.Serialization
 {
     public static class JsonClone
     {
+        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings();
+
+        static JsonClone()
+        {
+            Settings.DefaultValueHandling = DefaultValueHandling.Ignore;
+            Settings.NullValueHandling = NullValueHandling.Ignore;
+            Settings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+        }
+
+
         public static T Clone<T>(this T source) where T : new()
         {
-            var json = Json.Serialize(source);
-            return Json.Deserialize<T>(json);
+            var json = JsonConvert.SerializeObject(source, Settings);
+            return JsonConvert.DeserializeObject<T>(json, Settings);
         }
     }
 }
