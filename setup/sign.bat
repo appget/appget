a2d0e14d14ebff4317846e5271bb23004e1023db
Keivan <me@keivan.io>
2019-04-18 20:04:11 -0700

    Updated Authenticode cert name

--- setup/sign.bat
+++ setup/sign.bat
@@ -4,7 +4,7 @@ setup\AzureSignTool\AzureSignTool.exe sign ^
 --azure-key-vault-url %AKV_URL% ^
 --azure-key-vault-client-id %AKV_CLIENT_ID% ^
 --azure-key-vault-client-secret %AKV_CLIENT_SECRET% ^
---azure-key-vault-certificate appget-software ^
+--azure-key-vault-certificate appget-authenticode ^
 --timestamp-rfc3161 http://timestamp.digicert.com ^
 --timestamp-digest sha512 ^
 -v ^
